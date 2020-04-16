9680e078ac71c62552f00b9360903ff73798b198
suxxus <suxxus@outlook.com>
2020-04-16 14:05:42 +0200

    Create example object decoder

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,49 @@
-export default function sum(a: number, b: number) {
-  return a + b;
-}
+import * as D from 'json-decoder';
 
-console.log(sum(1, 3));
+type UserAddress = {
+  streetName: string;
+  streetNum: number;
+};
+
+type User = {
+  firstName: string;
+  lastName: string;
+  address: UserAddress;
+  email?: string;
+};
+
+const userAddressDecoder = D.objectDecoder<UserAddress>({
+  streetName: D.stringDecoder,
+  streetNum: D.numberDecoder,
+});
+
+const userDecoder = D.objectDecoder<User>({
+  firstName: D.stringDecoder,
+  lastName: D.stringDecoder,
+  address: userAddressDecoder,
+  email: D.oneOfDecoders(D.stringDecoder, D.undefinedDecoder),
+});
+
+const jsonUser = {
+  firstName: 'Juan',
+  lastName: 'Perez',
+  address: {
+    streetName: 'San Paolo',
+    streetNum: 1,
+  },
+  email: 'j.perez@xx.com',
+};
+
+const user = userDecoder.decode(jsonUser);
+
+const toDecodedUser = (r: D.Result<User>) => {
+  switch (r.type) {
+    case 'OK':
+      return r.value;
+    case 'ERR':
+      return r.message;
+  }
+};
+
+console.log(user);
+console.log('user type result: ', toDecodedUser(user));
